393f2ce4ff4bd25e2ccd3a2269754b2ea3b8f4d5
Gustavo Paes <gustavo910@gmail.com>
2025-04-09 15:57:58 -0300

    feat: start config

---
+++ workspace_gateway-api/gateway-api/src/main/kotlin/com/ajudaqui/gateway_api/GatewayApiApplication.kt
@@ -0,0 +1,11 @@
+package com.ajudaqui.gateway_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayApiApplication>(*args)
+}
---
+++ workspace_gateway-api/gateway-api/src/test/kotlin/com/ajudaqui/gateway_api/GatewayApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ajudaqui.gateway_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
